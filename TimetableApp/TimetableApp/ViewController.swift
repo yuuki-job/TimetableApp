8c90d0945bd820c8d243f806ef2a846f86b422f6
yuuki <ty0427s8145460@icloud.com>
2021-04-23 18:35:30 +0900

    初期コミット

---
+++ TimetableApp/TimetableApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimetableApp
+//
+//  Created by 徳永勇希 on 2021/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
